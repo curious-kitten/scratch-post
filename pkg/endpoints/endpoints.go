6f6b072cb61877f13443a41b41aef191107ff5f6
mimatache <matache91mh@gmail.com>
2021-01-03 11:50:19 +0200

    Added endpoints package to handle generic HTTP requests and moved item logic in each item

---
+++ pkg/endpoints/endpoints.go
@@ -0,0 +1,124 @@
+package endpoints
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+
+	"github.com/curious-kitten/scratch-post/internal/http/authorization"
+	"github.com/curious-kitten/scratch-post/internal/http/helpers"
+	"github.com/curious-kitten/scratch-post/internal/store"
+	"github.com/curious-kitten/scratch-post/pkg/metadata"
+)
+
+type create func(ctx context.Context, user string, body io.ReadCloser) (interface{}, error)
+
+// Creator reponds to a HTTP Post request to a collection
+func Creator(ctx context.Context, createFunc create, r *mux.Router) {
+	c := func(w http.ResponseWriter, r *http.Request) {
+		user, err := authorization.GetUserIDFromRequest(r)
+		if err != nil {
+			helpers.FormatError(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		toctx, cancel := context.WithTimeout(ctx, time.Second*10)
+		defer cancel()
+
+		item, err := createFunc(toctx, user, r.Body)
+		if err != nil {
+			handleError(err, w)
+			return
+		}
+		helpers.FormatResponse(w, item, http.StatusCreated)
+	}
+	r.HandleFunc("", c).Methods(http.MethodPost)
+	r.HandleFunc("/", c).Methods(http.MethodPost)
+}
+
+type list func(ctx context.Context) ([]interface{}, error)
+
+// Lister reponds to a HTTP Get request for a collection
+func Lister(ctx context.Context, listFunc list, r *mux.Router) {
+	l := func(w http.ResponseWriter, r *http.Request) {
+		toctx, cancel := context.WithTimeout(ctx, time.Second*10)
+		defer cancel()
+		items, err := listFunc(toctx)
+		if err != nil {
+			helpers.FormatError(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		itemList := &ItemList{
+			Count:      len(items),
+			TotalCount: len(items),
+			StartIndex: 0,
+			EndIndex:   len(items) - 1,
+			Items:      items,
+		}
+		helpers.FormatResponse(w, itemList, http.StatusOK)
+	}
+	r.HandleFunc("", l).Methods(http.MethodGet)
+	r.HandleFunc("/", l).Methods(http.MethodGet)
+}
+
+// ItemList formats the collection get response to a list
+type ItemList struct {
+	Count      int           `json:"count"`
+	TotalCount int           `json:"totalCount"`
+	StartIndex int           `json:"startIndex"`
+	EndIndex   int           `json:"endIndex"`
+	Items      []interface{} `json:"items"`
+}
+
+type get func(ctx context.Context, id string) (interface{}, error)
+
+// Getter returns a single instance of an item based on the ID in the path
+func Getter(ctx context.Context, getterFunc get, r *mux.Router) {
+	i := func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		id := params["id"]
+		toctx, cancel := context.WithTimeout(ctx, time.Second*10)
+		defer cancel()
+		item, err := getterFunc(toctx, id)
+		if err != nil {
+			handleError(err, w)
+			return
+		}
+		helpers.FormatResponse(w, item, http.StatusOK)
+	}
+	r.HandleFunc("/{id}", i).Methods(http.MethodGet)
+}
+
+type deleteItem func(ctx context.Context, id string) error
+
+// Deleter provides an API endpoint used to delete an intem
+func Deleter(ctx context.Context, deleterFunc deleteItem, r *mux.Router) {
+	d := func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+		id := params["id"]
+		toctx, cancel := context.WithTimeout(ctx, time.Second*10)
+		defer cancel()
+		if err := deleterFunc(toctx, id); err != nil {
+			handleError(err, w)
+			return
+		}
+		helpers.FormatResponse(w, struct{ item string }{item: id}, http.StatusOK)
+	}
+	r.HandleFunc("/{id}", d).Methods(http.MethodDelete)
+}
+
+func handleError(err error, w http.ResponseWriter) {
+	switch {
+	case store.IsNotFoundError(err):
+		helpers.FormatError(w, "could not find requested item", http.StatusNotFound)
+	case metadata.IsValidationError(err):
+		helpers.FormatError(w, err.Error(), http.StatusBadRequest)
+	case store.IsDuplicateError(err):
+		helpers.FormatError(w, "item already exists", http.StatusBadRequest)
+	default:
+		helpers.FormatError(w, err.Error(), http.StatusInternalServerError)
+	}
+}
